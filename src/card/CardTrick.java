43b1e644b1add2f70e3e9c03c021d987eaf7aa58
JonPCortez <cortjonp@sheridancollege.ca>
2023-09-26 22:10:41 -0400

    student version 1 code (Jon Paul Cortez)

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,28 +4,62 @@
  */
 package card;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ * @author JpCor
+ *
  */
+
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(generateRandomSuit());
+            c.setValue(generateRandomValue());
+            magicHand[i] = c;
+            System.out.println(magicHand[i].getSuit() + " " + magicHand[i].getValue());
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+
+       
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a card value (1-13): ");
+        int userValue = scanner.nextInt();
+        System.out.print("Enter a card suit (diamonds, hearts, clubs, spades): ");
+        String userSuit = scanner.next();
+
+        Card userCard = new Card();
+        userCard.setValue(userValue);
+        userCard.setSuit(userSuit);
+
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == userCard.getValue() && card.getSuit().equals(userCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Congratulations! Your card was chosen");
+        } else {
+            System.out.println("Sorry, better luck next timee!!");
+        }
     }
-    
+
+             private static String generateRandomSuit() {
+        String[] suits = {"Hearts", "Diamonds", "Spades", "Clubs"};
+        int randomIndex = (int) (Math.random() * suits.length);
+        return suits[randomIndex];
+    }
+
+     private static int generateRandomValue() {
+        return (int) (Math.random() * 13) + 1;
+    }
 }
